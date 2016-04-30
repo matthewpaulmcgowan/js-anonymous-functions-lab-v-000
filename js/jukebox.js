22f5812328b09ccb1e2bb6df2e35eb6fa5f3696b
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-04-30 17:23:44 -0500

    Done.

--- js/jukebox.js
+++ js/jukebox.js
@@ -15,30 +15,37 @@ function randomNumber() {
 }
 
 function menu() {
-  $('#menu').on('click', function(e) {
-    $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  });
+  $('#menu').on('click', a);
+}
+
+function a(e) {
+  $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
 }
 
 function play() {
-  $('#play').on('click', function(e) {
-    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
-    return "now playing " + songToPlay + " by " + theArtist;
-  });
+  $('#play').on('click', b);
+}
+
+function b(e) {
+  $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+  return "now playing " + songToPlay + " by " + theArtist;
 }
 
 function pause() {
-  $('#pause').on('click', function(e) {
-    $('#songPaused').html(songToPlay + " is paused");
-  });
+  $('#pause').on('click', c);
 }
 
+function c(e) {
+  $('#songPaused').html(songToPlay + " is paused");
+}
 function show() {
-  $('#show').on('click', function(e) {
-    var songs = "";
-    for (var key in jukeboxSongs) {
-      songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
-    }
-    $('#showSongs').html(songs);
-  });
+  $('#show').on('click',d);
+}
+
+function d(e) {
+ var songs = "";
+ for (var key in jukeboxSongs) {
+   songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
+ }
+ $('#showSongs').html(songs);
 }
